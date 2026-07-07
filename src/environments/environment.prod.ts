8b2731e4e0cef92cfdfc13670c80d180458b52f9
Jefferson <jeffer230@gmail.com>
2026-07-07 08:39:53 -0500

    secure firebase api keys and ignore environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_PROYECTO.firebaseapp.com",
+    projectId: "TU_PROYECTO",
+    storageBucket: "TU_PROYECTO.appspot.com",
+    messagingSenderId: "TU_ID",
+    appId: "TU_APP_ID"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDjis3B-od_AwaeZgyAEFxM84VIor8T6GM",
-    authDomain: "to-do-list-app-8c237.firebaseapp.com",
-    projectId: "to-do-list-app-8c237",
-    storageBucket: "to-do-list-app-8c237.firebasestorage.app",
-    messagingSenderId: "13244648153",
-    appId: "1:13244648153:web:99908c03c159826a65ccc8",
+    apiKey: "TU_API_KEY",
+    authDomain: "TU_PROYECTO.firebaseapp.com",
+    projectId: "TU_PROYECTO",
+    storageBucket: "TU_PROYECTO.appspot.com",
+    messagingSenderId: "TU_ID",
+    appId: "TU_APP_ID"
   }
 };
