8b0df21c0351d304c1ffbd4e0a9d3f36e6330e2a
Jefferson <jeffer230@gmail.com>
2026-07-06 21:16:29 -0500

    implement firebase remote config service

---
+++ src/app/core/services/remote-config.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { initializeApp } from 'firebase/app';
+import { getRemoteConfig, fetchAndActivate, getValue } from 'firebase/remote-config';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RemoteConfigService {
+  // Inicializamos en 'false' por defecto
+  private enableCategoriesSubject = new BehaviorSubject<boolean>(false);
+  public enableCategories$: Observable<boolean> = this.enableCategoriesSubject.asObservable();
+
+  constructor() {
+    this.initRemoteConfig();
+  }
+
+  private async initRemoteConfig() {
+    try {
+      const app = initializeApp(environment.firebase);
+      const remoteConfig = getRemoteConfig(app);
+
+      // Valores por defecto Offline o antes del primer fetch
+      remoteConfig.defaultConfig = {
+        'enable_categories': false
+      };
+
+      // Reducir el caché a 0 para ver el cambio inmediato.
+      remoteConfig.settings.minimumFetchIntervalMillis = 0;
+
+      // Descargar valores de la nube y activarlos
+      await fetchAndActivate(remoteConfig);
+      
+      // Leer el valor y actualizar nuestro estado reactivo
+      const isEnabled = getValue(remoteConfig, 'enable_categories').asBoolean();
+      this.enableCategoriesSubject.next(isEnabled);
+
+    } catch (error) {
+      console.error('Error al inicializar Firebase Remote Config', error);
+      // Falla silenciosamente. La app sigue funcionando con el valor por defecto (false)
+    }
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDjis3B-od_AwaeZgyAEFxM84VIor8T6GM",
+    authDomain: "to-do-list-app-8c237.firebaseapp.com",
+    projectId: "to-do-list-app-8c237",
+    storageBucket: "to-do-list-app-8c237.firebasestorage.app",
+    messagingSenderId: "13244648153",
+    appId: "1:13244648153:web:99908c03c159826a65ccc8",
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
